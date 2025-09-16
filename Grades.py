45d1ff06cb8763e45993d9665dd51f34cf38c368
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-09-16 21:36:23 +0530

    Create Grades.py using 3 different patterns

---
+++ Grades.py
@@ -0,0 +1,25 @@
+#If grade is greater than or equal to 55, then print "You passed."
+#Else, print "You failed."
+
+#pattern 1:
+grade = 40.23
+if grade >= 55: 
+    print('You passed') 
+else:
+    print('You failed')
+
+#pattern 2:
+grade = int(input('enter marks value: ')) 
+if grade >= 55: 
+    print('You passed') 
+else:
+    print('You failed')
+
+#pattern 3:        
+import random
+grade = random.randint(0, 100)
+print(grade)
+if grade >= 55:
+  print("YOU PASSED")
+else:
+  print("YOU FAILED")
