35ba98cfcb32feebc711672a83b41d4d0f9967f1
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-09-17 13:42:11 +0530

    Update Coin_flip.py using if-else condition

--- 2.Coin_flip.py
+++ 2.Coin_flip.py
@@ -1,6 +1,6 @@
 import random
 
-num = random.randint(0, 1)   # Generates a random number that's either 0 or 1
+num = random.randint(0, 1)   # it will Generate a random number that's either 0 or 1
 
 if num > 0.5:
   print('Heads')
