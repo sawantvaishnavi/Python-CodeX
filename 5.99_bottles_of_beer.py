fae0f71544658ada2ba32005736ad8e32437a781
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-09-17 13:47:26 +0530

    99_bottles_of_beer.py song created using string interpolation.

--- 5.99_bottles_of_beer.py
+++ 5.99_bottles_of_beer.py
@@ -1,11 +1,3 @@
-# Create a 99_bottles.py program and use a for loop and a range() function to print out all the verses of "99 Bottles of Beer."
-
-# 99 bottles of beer on the wall
-# 99 bottles of beer
-# Take one down, pass it around
-# 98 bottles of beer on the wall
-
-
 for i in range (99, 0, -1):
     print(f'{i} bottles of beer on the wall')
     print(f'{i} bottles of beer')
