a28192aa26a7e849a36a88958330c30e5b6855a2
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-09-17 13:48:20 +0530

    FizzBuzz.py

--- 6.FizzBuzz.py
+++ 6.FizzBuzz.py
@@ -1,10 +1,3 @@
-# Create a fizz_buzz.py program that outputs numbers from 1 to 100.
-
-# Here's the catch:
-# For multiples of 3, print "Fizz" instead of the number.
-# For multiples of 5, print "Buzz" instead of the number.
-# Here's the tricky part: For multiples of 3 and 5, print "FizzBuzz".
-
 for x in range (1,101, 1):
     if x%3==0 and x%5==0:
         print('FizzBuzz')
