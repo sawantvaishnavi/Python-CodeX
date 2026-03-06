f3bbed2099dfff9c220d21316e7d01443c2ecb77
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-03-06 21:08:58 +0530

    Update 2.Regex.py

--- Script/2.Regex.py
+++ Script/2.Regex.py
@@ -1,4 +1,12 @@
-# 1. To search ERROR in logfile
+#1. To search ERROR in logfile and print the results in terminal
+import re
+with open ("file2.txt", "r") as file:
+    for line in file:
+        if re.search(r"ERROR", line):
+            print(line)
+            
+
+#2. To search ERROR in logfile and write into new output file
 import re
 with open ("file2.txt", "r") as infile , open ("Summary.txt", "w") as outfile:
     for line in infile:
@@ -7,7 +15,7 @@ with open ("file2.txt", "r") as infile , open ("Summary.txt", "w") as outfile:
             outfile.write(line)
 
 
-#2. To search ERROR or WARNING
+#3. To search ERROR or WARNING
 import re
 with open ("file2.txt", "r") as infile , open ("Summary.txt", "w") as outfile:
     for line in infile:
@@ -15,3 +23,11 @@ with open ("file2.txt", "r") as infile , open ("Summary.txt", "w") as outfile:
             print(line)
             outfile.write(line)
       
+#4. To search Slack (VIOLATED) this pattern  -- r"Pattern"  this r means raw string. consider lines as plain text. 
+#if name\nAddress is there then that \n will be consider ans just plain text instade of new line.
+import re
+with open ("file2.txt", "r") as file:
+    for line in file:
+        if re.search(r"Slack \(VIOLATED\)", line):
+            print(line)
+            
